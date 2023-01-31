f5882d67cec827363ac894fd6099b534230f232b
Leo Chen <4513927+LeoHChen@users.noreply.github.com>
2023-01-30 23:48:15 -0800

    Update redirectorDB.js
    added linkedin

--- redirectorDB.js
+++ redirectorDB.js
@@ -9,5 +9,6 @@ var redirectorDB = {
     "twitter": 'https://twitter.com/leo_hao',
     "zoom": 'https://zoom.us/j/9867510240',
     "meet": 'https://meet.google.com/cec-xnpg-bks',
-    "cal": 'https://calendar.google.com/calendar/u/0?cid=bGVvQHN0b3J5cHJvdG9jb2wueHl6'
+    "cal": 'https://calendar.google.com/calendar/u/0?cid=bGVvQHN0b3J5cHJvdG9jb2wueHl6',
+    "linkedin": 'https://www.linkedin.com/in/haochen/'
 };
