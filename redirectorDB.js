6fccf18b9c393cb53bde90606726de1f70d359c5
Leo Chen <4513927+LeoHChen@users.noreply.github.com>
2023-01-30 23:33:56 -0800

    Create redirectorDB.js

---
+++ redirectorDB.js
@@ -0,0 +1,13 @@
+/**
+ * All links NEED http:// in-front to work
+ * Links with special chars need to be in single quotes
+ * redirectorDB is just a javascript object so js syntax applies but be consistent.
+ *
+ */
+
+var redirectorDB = {
+    "twitter": 'https://twitter.com/leo_hao',
+    "zoom": 'https://zoom.us/j/9867510240',
+    "meet": 'meet.google.com/cec-xnpg-bks',
+    "cal": 'https://calendar.google.com/calendar/u/0?cid=bGVvQHN0b3J5cHJvdG9jb2wueHl6'
+};
